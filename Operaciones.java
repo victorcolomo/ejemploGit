78f96d9e155cab5123182e9856ded70f4b3bbf51
Victor Colomo <victor.colomo@gmail.com>
2025-01-02 17:54:05 +0100

    Implementada multiplicación

--- Operaciones.java
+++ Operaciones.java
@@ -3,8 +3,9 @@ public class Operaciones {
         return a + b;
     }
     public static int resta(int a, int b) {
-        return a  b;
+        return a - b;
     }
-
-
+    public static int multiplicacion(int a, int b) {
+        return a * b;
+    }
 }
